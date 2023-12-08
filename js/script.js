f916fdd6ef3ec9869c2906bbad59f537bb4ef57b
Christian Sergerie <christian.sergerie@gmail.com>
2023-12-07 22:34:42 -0500

    finished media queries and mobile nav was added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
+// Mobile navigation
+const mainMenu = document.querySelector(".menu");
+const closeMenu = document.querySelector(".closeMenu");
+const openMenu = document.querySelector(".openMenu");
+const menu_items = document.querySelectorAll("nav .menu li a");
 
+openMenu.addEventListener("click", show);
+closeMenu.addEventListener("click", close);
+
+// close menu when you click on a menu item
+menu_items.forEach((item) => {
+  item.addEventListener("click", function () {
+    close();
+    console.log("items");
+  });
+});
+
+function show() {
+  mainMenu.style.display = "flex";
+  mainMenu.style.top = "0";
+  console.log("opened");
+}
+function close() {
+  mainMenu.style.top = "-100%";
+  console.log("closed");
+}
